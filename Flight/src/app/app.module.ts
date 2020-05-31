ddc51cd659a5a18c56fe83ba54f89cc6d4966839
kacharerohan <kacharerohan@gmail.com>
2020-05-31 17:34:08 +0530

    usercomponent

--- Flight/src/app/app.module.ts
+++ Flight/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
