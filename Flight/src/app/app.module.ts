4bdf924b7c519a2d3cc957a9ce57b0bc97dbd4bb
MohammedThouseef <thouseef31398@gmail.com>
2020-05-31 17:44:47 +0530

    flight

--- Flight/src/app/app.module.ts
+++ Flight/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddflightComponent } from './addflight/addflight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddflightComponent
   ],
   imports: [
     BrowserModule,
