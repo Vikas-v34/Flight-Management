75764b963065f8deba5237769dfe937e175c09e2
kacharerohan <kacharerohan@gmail.com>
2020-05-31 19:00:15 +0530

    tsfilesuser

--- Flight/src/app/user-details/user-details.component.ts
+++ Flight/src/app/user-details/user-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  userId: number;
+  user: User;
+  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.user= new User();
+    this.userId = this.route.snapshot.params['userId'];
+
+    this.userService.getUser(this.userId)
+    .subscribe(data => {console.log(data)
+    this.user = data;
+    },
+    error => console.log(error));
   }
 
+  list(){
+    this.router.navigate(['users']);
+  }
 }
